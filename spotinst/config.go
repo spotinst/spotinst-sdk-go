b2e2856e2252dd13f694eb5ff4df247cb4da445d
talzur <tal@spotinst.com>
2017-03-14 20:15:25 +0200

    Starting version 2.0.5

--- spotinst/config.go
+++ spotinst/config.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// SDKVersion is the current version of the SDK.
-	SDKVersion = "2.0.4"
+	SDKVersion = "2.0.5"
 
 	// SDKName is the name of the SDK.
 	SDKName = "spotinst-sdk-go"
