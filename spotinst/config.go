fa828b04fd0994b86e0e652a8b2a8fe89dd545a9
talzur <tal@spotinst.com>
2017-03-16 14:58:43 +0200

    version: Bump version to 2.0.11

--- spotinst/config.go
+++ spotinst/config.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// SDKVersion is the current version of the SDK.
-	SDKVersion = "2.0.10"
+	SDKVersion = "2.0.11"
 
 	// SDKName is the name of the SDK.
 	SDKName = "spotinst-sdk-go"
