d5c51ba1278e9a6f0e51d38f7ff0fdd6480ccfb5
Liran Polak <liran.polak@gmail.com>
2017-04-18 08:57:33 +0300

    Bump version to 2.0.19

--- spotinst/config.go
+++ spotinst/config.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// SDKVersion is the current version of the SDK.
-	SDKVersion = "2.0.18"
+	SDKVersion = "2.0.19"
 
 	// SDKName is the name of the SDK.
 	SDKName = "spotinst-sdk-go"
