4d431f9cac5487009aa23b088dc12164ed7d12fc
Thorsteinn Thorri Sigurdsson <thorsteinnth@gmail.com>
2021-03-01 08:18:50 +0100

    spark application APIs

--- service/wave/wave.go
+++ service/wave/wave.go
@@ -47,6 +47,7 @@ type Component struct {
 	State           *string           `json:"state,omitempty"`
 }
 
+// TODO Remove this I think - just let the API return a validation error
 type ClusterState string
 
 const (
@@ -56,6 +57,22 @@ const (
 	ClusterFailing     ClusterState = "FAILING"
 	ClusterUnknown     ClusterState = "UNKNOWN"
 )
+
+type SparkApplication struct {
+	ID                *string `json:"id,omitempty"`
+	ApplicationID     *string `json:"applicationId,omitempty"`
+	ClusterIdentifier *string `json:"clusterIdentifier,omitempty"`
+	Name              *string `json:"name,omitempty"`
+	Namespace         *string `json:"namespace,omitempty"`
+	Heritage          *string `json:"heritage,omitempty"`
+	ApplicationState  *string `json:"applicationState,omitempty"`
+
+	// TODO More fields?
+
+	// Read-only fields.
+	CreatedAt *time.Time `json:"createdAt,omitempty"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty"`
+}
 
 type ListClustersInput struct {
 	ClusterIdentifier *string       `json:"clusterIdentifier,omitempty"`
@@ -81,6 +98,27 @@ type DeleteClusterInput struct {
 }
 
 type DeleteClusterOutput struct{}
+
+type ListSparkApplicationsInput struct {
+	ClusterIdentifier *string `json:"clusterIdentifier,omitempty"`
+	Name              *string `json:"name,omitempty"`
+	Namespace         *string `json:"namespace,omitempty"`
+	ApplicationId     *string `json:"applicationId,omitempty"`
+	ApplicationState  *string `json:"applicationState,omitempty"`
+	Heritage          *string `json:"heritage,omitempty"`
+}
+
+type ListSparkApplicationsOutput struct {
+	SparkApplications []*SparkApplication `json:"sparkApplications,omitempty"`
+}
+
+type ReadSparkApplicationInput struct {
+	ID *string `json:"id,omitempty"`
+}
+
+type ReadSparkApplicationOutput struct {
+	SparkApplication *SparkApplication `json:"sparkApplication,omitempty"`
+}
 
 func (s *ServiceOp) ListClusters(ctx context.Context, input *ListClustersInput) (*ListClustersOutput, error) {
 	r := client.NewRequest(http.MethodGet, "/wave/cluster")
@@ -195,6 +233,112 @@ func clustersFromJSON(in []byte) ([]*Cluster, error) {
 
 func clusterFromJSON(in []byte) (*Cluster, error) {
 	b := new(Cluster)
+	if err := json.Unmarshal(in, b); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *ServiceOp) ListSparkApplications(ctx context.Context, input *ListSparkApplicationsInput) (*ListSparkApplicationsOutput, error) {
+	r := client.NewRequest(http.MethodGet, "/wave/spark/application")
+
+	if input != nil {
+		if input.ClusterIdentifier != nil {
+			r.Params.Set("clusterIdentifier", spotinst.StringValue(input.ClusterIdentifier))
+		}
+
+		if input.Name != nil {
+			r.Params.Set("name", spotinst.StringValue(input.Name))
+		}
+
+		if input.ApplicationId != nil {
+			r.Params.Set("applicationId", spotinst.StringValue(input.ApplicationId))
+		}
+
+		if input.ApplicationState != nil {
+			r.Params.Set("applicationState", spotinst.StringValue(input.ApplicationState))
+		}
+
+		if input.Heritage != nil {
+			r.Params.Set("heritage", spotinst.StringValue(input.Heritage))
+		}
+
+		if input.Namespace != nil {
+			r.Params.Set("namespace", spotinst.StringValue(input.Namespace))
+		}
+	}
+
+	resp, err := client.RequireOK(s.Client.Do(ctx, r))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	sparkApplications, err := sparkApplicationsFromHttpResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ListSparkApplicationsOutput{SparkApplications: sparkApplications}, nil
+}
+
+func (s *ServiceOp) ReadSparkApplication(ctx context.Context, input *ReadSparkApplicationInput) (*ReadSparkApplicationOutput, error) {
+	path, err := uritemplates.Expand("/wave/spark/application/{id}", uritemplates.Values{
+		"id": spotinst.StringValue(input.ID),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	r := client.NewRequest(http.MethodGet, path)
+	resp, err := client.RequireOK(s.Client.Do(ctx, r))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	sparkApplications, err := sparkApplicationsFromHttpResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	output := new(ReadSparkApplicationOutput)
+	if len(sparkApplications) > 0 {
+		output.SparkApplication = sparkApplications[0]
+	}
+
+	return output, nil
+}
+
+func sparkApplicationsFromHttpResponse(resp *http.Response) ([]*SparkApplication, error) {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return sparkApplicationsFromJSON(body)
+}
+
+func sparkApplicationsFromJSON(in []byte) ([]*SparkApplication, error) {
+	var rw client.Response
+	if err := json.Unmarshal(in, &rw); err != nil {
+		return nil, err
+	}
+	out := make([]*SparkApplication, len(rw.Response.Items))
+	if len(out) == 0 {
+		return out, nil
+	}
+	for i, rb := range rw.Response.Items {
+		b, err := sparkApplicationFromJSON(rb)
+		if err != nil {
+			return nil, err
+		}
+		out[i] = b
+	}
+	return out, nil
+}
+
+func sparkApplicationFromJSON(in []byte) (*SparkApplication, error) {
+	b := new(SparkApplication)
 	if err := json.Unmarshal(in, b); err != nil {
 		return nil, err
 	}
