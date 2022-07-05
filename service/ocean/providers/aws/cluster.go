be16da03564b8fd97ec17a9d35ede684745c6fe5
TalGevaSpot <98812761+TalGevaSpot@users.noreply.github.com>
2022-07-05 13:15:51 +0300

    feat(ocean/aws): support respectPDB in roll API call(#196)

--- service/ocean/providers/aws/cluster.go
+++ service/ocean/providers/aws/cluster.go
@@ -294,6 +294,7 @@ type RollSpec struct {
 	Status                       *string  `json:"status,omitempty"`
 	BatchSizePercentage          *int     `json:"batchSizePercentage,omitempty"`
 	BatchMinHealthyPercentage    *int     `json:"batchMinHealthyPercentage,omitempty"`
+	RespectPDB                   *bool    `json:"respectPdb,omitempty"`
 	DisableLaunchSpecAutoScaling *bool    `json:"disableLaunchSpecAutoScaling,omitempty"`
 	LaunchSpecIDs                []string `json:"launchSpecIds,omitempty"`
 	InstanceIDs                  []string `json:"instanceIds,omitempty"`
@@ -1706,6 +1707,13 @@ func (o *RollSpec) SetBatchSizePercentage(v *int) *RollSpec {
 func (o *RollSpec) SetBatchMinHealthyPercentage(v *int) *RollSpec {
 	if o.BatchMinHealthyPercentage = v; o.BatchMinHealthyPercentage == nil {
 		o.nullFields = append(o.nullFields, "BatchMinHealthyPercentage")
+	}
+	return o
+}
+
+func (o *RollSpec) SetRespectPDB(v *bool) *RollSpec {
+	if o.RespectPDB = v; o.RespectPDB == nil {
+		o.nullFields = append(o.nullFields, "RespectPDB")
 	}
 	return o
 }
