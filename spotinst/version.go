b23f34e0ffb6be9ec7f914a3103cae2fbdc5cea6
Anurag Sharma <anurag4@netapp.com>
2023-06-01 00:17:39 +0530

    chore(release): v1.163.0

--- spotinst/version.go
+++ spotinst/version.go
@@ -1,7 +1,7 @@
 package spotinst
 
 // SDKVersion is the current version of the SDK.
-const SDKVersion = "1.162.0"
+const SDKVersion = "1.163.0"
 
 // SDKName is the name of the SDK.
 const SDKName = "spotinst-sdk-go"
