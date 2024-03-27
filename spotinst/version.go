2ed7604fa19376ec62151e0031ba5bc50fe3bbea
chandra1-n <86221454+chandra1-n@users.noreply.github.com>
2024-03-27 11:52:33 +0530

    chore(release): v1.338.0

--- spotinst/version.go
+++ spotinst/version.go
@@ -1,7 +1,7 @@
 package spotinst
 
 // SDKVersion is the current version of the SDK.
-const SDKVersion = "1.337.0"
+const SDKVersion = "1.338.0"
 
 // SDKName is the name of the SDK.
 const SDKName = "spotinst-sdk-go"
