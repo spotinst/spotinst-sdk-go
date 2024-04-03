05942b0ff8a8191b45a0249eba0f29f2366a00c5
patrapritish1 <59195856+patrapritish1@users.noreply.github.com>
2024-04-03 16:55:50 +0530

    added gpuTypes parameter

--- examples/service/ocean/providers/azure_np/nodegroup/create/main.go
+++ examples/service/ocean/providers/azure_np/nodegroup/create/main.go
@@ -102,6 +102,10 @@ func main() {
 						"GPU",
 					},
 					MinDisk: spotinst.Int(2),
+					GpuTypes: []string{
+						"nvidia-tesla-t4",
+						"nvidia-tesla-a100",
+					},
 				},
 			},
 		},
