4ccf6040dd26b0b3ff718bd23167e5ce28a7d2f4
Ishan Gude <104548702+IGUDE2@users.noreply.github.com>
2024-04-11 14:51:35 +0530

    Implemented Verification Provider and Service

---
+++ service/oceancd/service.go
@@ -0,0 +1,36 @@
+package oceancd
+
+import (
+	"context"
+
+	"github.com/spotinst/spotinst-sdk-go/spotinst"
+	"github.com/spotinst/spotinst-sdk-go/spotinst/client"
+	"github.com/spotinst/spotinst-sdk-go/spotinst/session"
+)
+
+// Service provides the API operation methods for making requests to endpoints
+// of the Spotinst API. See this package's package overview docs for details on
+// the service.
+type Service interface {
+	ListVerificationProviders(context.Context) (*ListVerificationProvidersOutput, error)
+	CreateVerificationProvider(context.Context, *CreateVerificationProviderInput) (*CreateVerificationProviderOutput, error)
+	ReadVerificationProvider(context.Context, *ReadVerificationProviderInput) (*ReadVerificationProviderOutput, error)
+	UpdateVerificationProvider(context.Context, *UpdateVerificationProviderInput) (*UpdateVerificationProviderOutput, error)
+	DeleteVerificationProvider(context.Context, *DeleteVerificationProviderInput) (*DeleteVerificationProviderOutput, error)
+}
+
+type ServiceOp struct {
+	Client *client.Client
+}
+
+var _ Service = &ServiceOp{}
+
+func New(sess *session.Session, cfgs ...*spotinst.Config) *ServiceOp {
+	cfg := &spotinst.Config{}
+	cfg.Merge(sess.Config)
+	cfg.Merge(cfgs...)
+
+	return &ServiceOp{
+		Client: client.New(sess.Config),
+	}
+}
