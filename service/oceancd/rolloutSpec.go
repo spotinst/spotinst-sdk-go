4ca257146fd3a9a334175f1639ced20ad6150a00
IGUDE2 <ishan.gude@netapp.com>
2024-05-17 13:01:36 +0530

    Fixed

--- service/oceancd/rolloutSpec.go
+++ service/oceancd/rolloutSpec.go
@@ -789,7 +789,7 @@ func (o DestinationRule) MarshalJSON() ([]byte, error) {
 	return jsonutil.MarshalJSON(raw, o.forceSendFields, o.nullFields)
 }
 
-func (o *DestinationRule) SetCanarySubseme(v *string) *DestinationRule {
+func (o *DestinationRule) SetCanarySubnetName(v *string) *DestinationRule {
 	if o.CanarySubsetName = v; o.CanarySubsetName == nil {
 		o.nullFields = append(o.nullFields, "CanarySubsetName")
 	}
