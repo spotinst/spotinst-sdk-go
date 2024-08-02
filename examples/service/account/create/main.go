24e152f4e1940c1997abc83989d8868c10256e26
Sharad Kesarwani <108344822+sharadkesarwani@users.noreply.github.com>
2024-08-02 12:42:05 +0530

    added common acct create code

--- examples/service/account/create/main.go
+++ examples/service/account/create/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"github.com/spotinst/spotinst-sdk-go/service/account"
-	"github.com/spotinst/spotinst-sdk-go/service/account/providers/aws"
+	"github.com/spotinst/spotinst-sdk-go/service/account/providers/common"
 	"github.com/spotinst/spotinst-sdk-go/spotinst"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/session"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/util/stringutil"
@@ -14,8 +14,8 @@ func main() {
 	sess := session.New()
 	svc := account.New(sess)
 	ctx := context.Background()
-	out, err := svc.CloudProviderAWS().CreateAccount(ctx, &aws.CreateAccountInput{
-		Account: &aws.Account{
+	out, err := svc.CloudProviderCommon().CreateAccount(ctx, &common.CreateAccountInput{
+		Account: &common.Account{
 			Name: spotinst.String("testAcct_123"),
 		},
 	})
--- service/account/account.go
+++ service/account/account.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"github.com/spotinst/spotinst-sdk-go/service/account/providers/aws"
+	"github.com/spotinst/spotinst-sdk-go/service/account/providers/common"
 	"github.com/spotinst/spotinst-sdk-go/service/account/providers/gcp"
 	"github.com/spotinst/spotinst-sdk-go/spotinst"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/client"
@@ -14,6 +15,7 @@ import (
 type Service interface {
 	CloudProviderAWS() aws.Service
 	CloudProviderGCP() gcp.Service
+	CloudProviderCommon() common.Service
 }
 
 type ServiceOp struct {
@@ -40,6 +42,12 @@ func (s *ServiceOp) CloudProviderAWS() aws.Service {
 
 func (s *ServiceOp) CloudProviderGCP() gcp.Service {
 	return &gcp.ServiceOp{
+		Client: s.Client,
+	}
+}
+
+func (s *ServiceOp) CloudProviderCommon() common.Service {
+	return &common.ServiceOp{
 		Client: s.Client,
 	}
 }
