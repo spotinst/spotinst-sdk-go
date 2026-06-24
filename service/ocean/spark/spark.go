82fa72fa5b708f3155561c07a7ea8b058e41b744
yasharma-spot-flexera <yasharma@flexera.com>
2026-06-24 15:47:34 +0530

    feat(ocean Spark): Removed support for OFAS APIs.

--- service/ocean/spark/spark.go
+++ service/ocean/spark/spark.go
@@ -3,46 +3,49 @@ package spark
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 
-	"github.com/spotinst/spotinst-sdk-go/spotinst"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/client"
-	"github.com/spotinst/spotinst-sdk-go/spotinst/util/uritemplates"
 )
+
+var ErrOFASDecommissioned = errors.New("OFAS (Ocean for Apache Spark) has been decommissioned and is no longer available")
 
 // region Cluster
 func (s *ServiceOp) ListClusters(ctx context.Context, input *ListClustersInput) (*ListClustersOutput, error) {
-	r := client.NewRequest(http.MethodGet, "/ocean/spark/cluster")
+	return nil, ErrOFASDecommissioned
 
-	if input != nil {
-		if input.ControllerClusterID != nil {
-			r.Params.Set("controllerClusterId", spotinst.StringValue(input.ControllerClusterID))
+	/*	r := client.NewRequest(http.MethodGet, "/ocean/spark/cluster")
+
+		if input != nil {
+			if input.ControllerClusterID != nil {
+				r.Params.Set("controllerClusterId", spotinst.StringValue(input.ControllerClusterID))
+			}
+
+			if input.ClusterState != nil {
+				r.Params.Set("state", spotinst.StringValue(input.ClusterState))
+			}
 		}
 
-		if input.ClusterState != nil {
-			r.Params.Set("state", spotinst.StringValue(input.ClusterState))
+		resp, err := client.RequireOK(s.Client.Do(ctx, r))
+		if err != nil {
+			return nil, err
 		}
-	}
+		defer resp.Body.Close()
 
-	resp, err := client.RequireOK(s.Client.Do(ctx, r))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+		clusters, err := clustersFromHttpResponse(resp)
+		if err != nil {
+			return nil, err
+		}
 
-	clusters, err := clustersFromHttpResponse(resp)
-	if err != nil {
-		return nil, err
-	}
-
-	return &ListClustersOutput{Clusters: clusters}, nil
+		return &ListClustersOutput{Clusters: clusters}, nil*/
 }
 
 func (s *ServiceOp) ReadCluster(ctx context.Context, input *ReadClusterInput) (*ReadClusterOutput, error) {
-	if input == nil {
+	return nil, ErrOFASDecommissioned
+
+	/*if input == nil {
 		return nil, fmt.Errorf("input is nil")
 	}
 
@@ -70,11 +73,13 @@ func (s *ServiceOp) ReadCluster(ctx context.Context, input *ReadClusterInput) (*
 		output.Cluster = clusters[0]
 	}
 
-	return output, nil
+	return output, nil*/
 }
 
 func (s *ServiceOp) DeleteCluster(ctx context.Context, input *DeleteClusterInput) (*DeleteClusterOutput, error) {
-	if input == nil {
+	return nil, ErrOFASDecommissioned
+
+	/*if input == nil {
 		return nil, fmt.Errorf("input is nil")
 	}
 
@@ -97,57 +102,60 @@ func (s *ServiceOp) DeleteCluster(ctx context.Context, input *DeleteClusterInput
 	}
 	defer resp.Body.Close()
 
-	return &DeleteClusterOutput{}, nil
+	return &DeleteClusterOutput{}, nil*/
 }
 
 func (s *ServiceOp) CreateCluster(ctx context.Context, input *CreateClusterInput) (*CreateClusterOutput, error) {
-	if input == nil {
+	return nil, ErrOFASDecommissioned
+
+	/*if input == nil {
 		return nil, fmt.Errorf("input is nil")
 	}
-	r := client.NewRequest(http.MethodPost, "/ocean/spark/cluster")
-	r.Obj = input
+		r := client.NewRequest(http.MethodPost, "/ocean/spark/cluster")
+		r.Obj = input
 
-	resp, err := client.RequireOK(s.Client.Do(ctx, r))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+		resp, err := client.RequireOK(s.Client.Do(ctx, r))
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
 
-	gs, err := clustersFromHttpResponse(resp)
-	if err != nil {
-		return nil, err
-	}
+		gs, err := clustersFromHttpResponse(resp)
+		if err != nil {
+			return nil, err
+		}
 
-	output := new(CreateClusterOutput)
-	if len(gs) > 0 {
-		output.Cluster = gs[0]
-	}
+		output := new(CreateClusterOutput)
+		if len(gs) > 0 {
+			output.Cluster = gs[0]
+		}
 
-	return output, nil
+		return output, nil*/
 }
 
 func (s *ServiceOp) UpdateCluster(ctx context.Context, input *UpdateClusterInput) (*UpdateClusterOutput, error) {
-	if input == nil {
+	return nil, ErrOFASDecommissioned
+
+	/* 	if input == nil {
 		return nil, fmt.Errorf("input is nil")
 	}
-
 	path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}", uritemplates.Values{
-		"clusterId": spotinst.StringValue(input.ClusterID),
-	})
-	if err != nil {
-		return nil, err
-	}
+			"clusterId": spotinst.StringValue(input.ClusterID),
+		})
+		if err != nil {
+			return nil, err
+		}
 
-	r := client.NewRequest(http.MethodPut, path)
-	r.Obj = input
+		r := client.NewRequest(http.MethodPut, path)
+		r.Obj = input
 
-	resp, err := client.RequireOK(s.Client.Do(ctx, r))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+		resp, err := client.RequireOK(s.Client.Do(ctx, r))
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
 
-	return &UpdateClusterOutput{}, nil
+		return &UpdateClusterOutput{}, nil*/
 }
 
 func clustersFromHttpResponse(resp *http.Response) ([]*Cluster, error) {
@@ -186,93 +194,96 @@ func clusterFromJSON(in []byte) (*Cluster, error) {
 
 // region Virtual Node Group
 func (s *ServiceOp) ListVirtualNodeGroups(ctx context.Context, input *ListVngsInput) (*ListVngsOutput, error) {
-	if input == nil {
-		return nil, fmt.Errorf("input is nil")
-	}
+	return nil, ErrOFASDecommissioned
 
-	path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}/virtualNodeGroup", uritemplates.Values{
-		"clusterId": spotinst.StringValue(input.ClusterID),
-	})
-	if err != nil {
-		return nil, err
-	}
+	/*if input == nil {
+	    		return nil, fmt.Errorf("input is nil")
+	    	}
+	  path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}/virtualNodeGroup", uritemplates.Values{
+	  		"clusterId": spotinst.StringValue(input.ClusterID),
+	  	})
+	  	if err != nil {
+	  		return nil, err
+	  	}
 
-	r := client.NewRequest(http.MethodGet, path)
-	resp, err := client.RequireOK(s.Client.Do(ctx, r))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+	  	r := client.NewRequest(http.MethodGet, path)
+	  	resp, err := client.RequireOK(s.Client.Do(ctx, r))
+	  	if err != nil {
+	  		return nil, err
+	  	}
+	  	defer resp.Body.Close()
 
-	vngs, err := vngsFromHttpResponse(resp)
-	if err != nil {
-		return nil, err
-	}
+	  	vngs, err := vngsFromHttpResponse(resp)
+	  	if err != nil {
+	  		return nil, err
+	  	}
 
-	output := new(ListVngsOutput)
-	if len(vngs) > 0 {
-		output.VirtualNodeGroups = vngs
-	}
+	  	output := new(ListVngsOutput)
+	  	if len(vngs) > 0 {
+	  		output.VirtualNodeGroups = vngs
+	  	}
 
-	return output, nil
+	  	return output, nil*/
 }
 
 func (s *ServiceOp) DetachVirtualNodeGroup(ctx context.Context, input *DetachVngInput) (*DetachVngOutput, error) {
-	if input == nil {
-		return nil, fmt.Errorf("input is nil")
-	}
+	return nil, ErrOFASDecommissioned
 
-	path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}/virtualNodeGroup/{vngId}", uritemplates.Values{
-		"clusterId": spotinst.StringValue(input.ClusterID),
-		"vngId":     spotinst.StringValue(input.VngID),
-	})
-	if err != nil {
-		return nil, err
-	}
+	/*	if input == nil {
+				return nil, fmt.Errorf("input is nil")
+			}
+		path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}/virtualNodeGroup/{vngId}", uritemplates.Values{
+				"clusterId": spotinst.StringValue(input.ClusterID),
+				"vngId":     spotinst.StringValue(input.VngID),
+			})
+			if err != nil {
+				return nil, err
+			}
 
-	r := client.NewRequest(http.MethodDelete, path)
+			r := client.NewRequest(http.MethodDelete, path)
 
-	resp, err := client.RequireOK(s.Client.Do(ctx, r))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+			resp, err := client.RequireOK(s.Client.Do(ctx, r))
+			if err != nil {
+				return nil, err
+			}
+			defer resp.Body.Close()
 
-	return &DetachVngOutput{}, nil
+			return &DetachVngOutput{}, nil*/
 }
 
 func (s *ServiceOp) AttachVirtualNodeGroup(ctx context.Context, input *AttachVngInput) (*AttachVngOutput, error) {
-	if input == nil {
-		return nil, fmt.Errorf("input is nil")
-	}
+	return nil, ErrOFASDecommissioned
 
-	path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}/virtualNodeGroup", uritemplates.Values{
-		"clusterId": spotinst.StringValue(input.ClusterID),
-	})
-	if err != nil {
-		return nil, err
-	}
+	/*	if input == nil {
+				return nil, fmt.Errorf("input is nil")
+			}
+		path, err := uritemplates.Expand("/ocean/spark/cluster/{clusterId}/virtualNodeGroup", uritemplates.Values{
+			"clusterId": spotinst.StringValue(input.ClusterID),
+		})
+		if err != nil {
+			return nil, err
+		}
 
-	r := client.NewRequest(http.MethodPost, path)
-	r.Obj = input
+		r := client.NewRequest(http.MethodPost, path)
+		r.Obj = input
 
-	resp, err := client.RequireOK(s.Client.Do(ctx, r))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+		resp, err := client.RequireOK(s.Client.Do(ctx, r))
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
 
-	gs, err := vngsFromHttpResponse(resp)
-	if err != nil {
-		return nil, err
-	}
+		gs, err := vngsFromHttpResponse(resp)
+		if err != nil {
+			return nil, err
+		}
 
-	output := new(AttachVngOutput)
-	if len(gs) > 0 {
-		output.VirtualNodeGroup = gs[0]
-	}
+		output := new(AttachVngOutput)
+		if len(gs) > 0 {
+			output.VirtualNodeGroup = gs[0]
+		}
 
-	return output, nil
+		return output, nil*/
 }
 
 func vngsFromHttpResponse(resp *http.Response) ([]*DedicatedVirtualNodeGroup, error) {
